a3610dae33b0c5d90a877493eb74d01e4aeaf488
Jeff Chapman <jeffcchapman@gmail.com>
2015-03-22 01:50:27 -0600

    added timeout and ngshows

--- public/js/templates/home/homeCtrl.js
+++ public/js/templates/home/homeCtrl.js
@@ -1,5 +1,13 @@
 var app = angular.module('app');
 
-app.controller('homeCtrl', function() {
-	
+app.controller('homeCtrl', function($timeout, $scope) {
+
+	$scope.textRun = function () {
+		$scope.text = true;
+	}
+
+	$timeout(function() {
+	        $scope.textRun();
+	}, 4000);
+
 })
